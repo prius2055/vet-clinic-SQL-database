fa27b7abbc82710d0a98ea5c7ec32e8e6fc46ca5
prius2055 <prius2055@gmail.com>
2023-06-07 18:24:47 +0100

    Update schema.sql file

--- schema.sql
+++ schema.sql
@@ -11,3 +11,43 @@
 
 /* Adding a column 'species' to the aninals table*/
 ALTER TABLE animals ADD species varchar(20);
+
+/* Creating owners table */
+vet_clinic=# CREATE TABLE owners (
+vet_clinic(# id SERIAL PRIMARY KEY,
+vet_clinic(# full_name TEXT,
+vet_clinic(# age INTEGER
+vet_clinic(# );
+CREATE TABLE
+vet_clinic=# CREATE TABLE species (
+vet_clinic(# id SERIAL PRIMARY KEY,
+vet_clinic(# name TEXT
+vet_clinic(# );
+CREATE TABLE
+
+/* Creating species table */
+CREATE TABLE owners (
+id SERIAL PRIMARY KEY,
+full_name TEXT,
+age INTEGER
+);
+
+CREATE TABLE species (
+id SERIAL PRIMARY KEY,
+name TEXT
+);
+
+
+/* Modifying animals table */
+
+ALTER TABLE animals DROP COLUMN species;
+ALTER TABLE animals ADD species_id INTEGER;
+
+ALTER TABLE animals
+ADD CONSTRAINT fk FOREIGN KEY (species_id) REFERENCES species (id);
+
+ALTER TABLE animals ADD owner_id INTEGER;
+
+ALTER TABLE animals
+ADD CONSTRAINT fk2 FOREIGN KEY (owner_id) REFERENCES owners (id);
+
