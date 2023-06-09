535ded03f788de7afa8aac2c4f67e6937cccce1e
prius2055 <prius2055@gmail.com>
2023-06-09 16:38:52 +0100

    Update data, queries and schema files

--- schema.sql
+++ schema.sql
@@ -40,3 +40,25 @@ ALTER TABLE animals ADD owner_id INTEGER;
 ALTER TABLE animals
 ADD CONSTRAINT fk2 FOREIGN KEY (owner_id) REFERENCES owners (id);
 
+
+/* Create a table named vets */
+ 
+CREATE TABLE vets(
+id SERIAL PRIMARY KEY,
+name TEXT,
+age INTEGER,
+date_of_graduation DATE
+);
+
+/* specializations table */
+CREATE TABLE specializations(
+species_id INTEGER, FOREIGN KEY(species_id) REFERENCES species(id),
+vets_id INTEGER, FOREIGN KEY(vets_id) REFERENCES vets(id)
+);
+
+/* visits table */
+CREATE TABLE visits(
+visit_dates DATE NOT NULL,
+animals_id INTEGER, FOREIGN KEY(animals_id) REFERENCES animals(id),
+vets_id INTEGER, FOREIGN KEY(vets_id) REFERENCES vets(id)
+);
